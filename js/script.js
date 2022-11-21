b753909925206ece71ee3e6cb8a217d50f191951
FlorianKomnino <florian.komnino@outlook.it>
2022-11-21 15:57:53 +0100

    added "if" to calculate the applied discount on tha base of the age

--- js/script.js
+++ js/script.js
@@ -5,4 +5,19 @@ let pricePerKm = 0.21;
 let tripLength = prompt ('Inserisci la lunghezza del tuo viaggio in kilometri');
 let userAge = prompt ('Inserisci la tua età');
 
-document.getElementById
+let ticketPrice = tripLength * 0.21;
+let dicountsApplied
+
+    if (userAge < 18) {
+        discountsApplied = 20 / 100;
+    } 
+    else if (userAge >= 65) {
+        discountsApplied = 40 / 100;
+    } 
+    else {
+        discountsApplied = 1;
+}
+
+
+
+document.getElementById('ticketPrice').innerHTML = tripLength * 0.21;
