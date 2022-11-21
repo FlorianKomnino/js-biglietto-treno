3edd4f07586670878ae20091f80567da1ab7c182
FlorianKomnino <florian.komnino@outlook.it>
2022-11-21 15:47:25 +0100

    added ids to html elements, added prompts and started populating script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
 console.log('Hello World!');
 
+let pricePerKm = 0.21;
+
+let tripLength = prompt ('Inserisci la lunghezza del tuo viaggio in kilometri');
+let userAge = prompt ('Inserisci la tua età');
+
+document.getElementById
