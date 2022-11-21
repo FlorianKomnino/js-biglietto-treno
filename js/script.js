9559bf7efa14d6865065786c8c0fad292850f910
FlorianKomnino <florian.komnino@outlook.it>
2022-11-21 16:33:07 +0100

    optimized script.js code

--- js/script.js
+++ js/script.js
@@ -6,16 +6,16 @@ let tripLength = prompt ('Inserisci la lunghezza del tuo viaggio in kilometri');
 let userAge = prompt ('Inserisci la tua età');
 
 let ticketPrice = tripLength * 0.21;
-let dicountsApplied = 1;
-document.getElementById('discountsApplied').innerHTML = '0&percnt;';
+let discountsApplied = 0;
+let printedPercentage = '0&percnt;'
 
     if (userAge < 18) {
         discountsApplied = 20 / 100;
-        document.getElementById('discountsApplied').innerHTML = '20&percnt;';
+        printedPercentage = '20&percnt;';
     } 
     else if (userAge >= 65) {
         discountsApplied = 40 / 100;
-        document.getElementById('discountsApplied').innerHTML = '40&percnt;';
+        printedPercentage = '40&percnt;';
     }
 
 console.log(ticketPrice);
@@ -23,6 +23,9 @@ console.log(discountsApplied);
 
 
 let finalTicketPrice = ticketPrice - (ticketPrice * discountsApplied);
+
+
+document.getElementById('discountsApplied').innerHTML = printedPercentage;
 
 document.getElementById('finalTicketPrice').innerHTML = finalTicketPrice.toFixed(2) + '&euro;';
 
